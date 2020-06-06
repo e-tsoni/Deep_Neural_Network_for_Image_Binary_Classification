c5363c0132ffb87228774746ec14fe388012bb6b
e-tsoni <etsoni.cs@gmail.com>
2020-06-06 23:43:10 +0300

    Reshape and Standardize the dataset / Add sigmoid.py

--- model_main.py
+++ model_main.py
@@ -5,4 +5,64 @@ Created on Sat Jun  6 22:23:52 2020
 @author: Eftychia Tsoni
 """
 
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+
+def load_dataset():
+    train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+
+    test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+
+    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+    
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+
+# Loading the data (cat / non-cat)
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+
+print(train_set_x_orig.shape)
+print(train_set_y.shape)
+print(test_set_x_orig.shape)
+print(test_set_y.shape)
+
+
+# Example of a picture
+index = 23
+plt.imshow(train_set_x_orig[index])
+plt.imshow(test_set_x_orig[index])
+
+# Finding the numbers of training examples, test examples, height=width of a picture
+m_train = train_set_x_orig.shape[0]
+m_test = test_set_x_orig.shape[0]
+num_px = train_set_x_orig.shape[1]
+
+print(m_train)
+print(m_test)
+print(num_px)
+
+# Reshape the training and test examples
+train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+
+print(train_set_x_flatten.shape)
+print(test_set_x_flatten.shape)
+
+# Standardize the dataset
+train_set_x = train_set_x_flatten/255
+test_set_x = test_set_x_flatten/255
+
+
+
+
+
+
 
---
+++ sigmoid.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  6 23:37:20 2020
+
+@author: Eftychia Tsoni
+"""
+
+def sigmoid(z):
+    """
+    Compute the sigmoid activation of linear z    
+
+    Parameters
+    ----------
+    z : a scalar or numpy array of any size
+
+    Returns
+    -------
+    s : the sigmoid function of z
+
+    """
+    
+    s = 1 / (1 + np.exp(-z))
+    
+    return s
